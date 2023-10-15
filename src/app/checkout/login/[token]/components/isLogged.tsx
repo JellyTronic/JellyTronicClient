ca17398e79fe629fea8686712066d5d307d9d26d
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-10-15 02:28:28 -0300

    feat: add funcition to checkout after login

---
+++ src/app/checkout/login/[token]/components/isLogged.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import "../components/isLogged.css";
+import { useRouter } from "next/navigation";
+
+export default function IsLogged() {
+  const [contador, setContador] = useState(3);
+
+  const router = useRouter();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/cart");
+    }, 3000);
+
+    const interval = setInterval(() => {
+      setContador((prevCount) => prevCount - 1);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+      clearInterval(interval);
+    };
+  }, [router]);
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <h1>Você está logado!</h1>
+        <br />
+        <h3>Redirecionando para a página de perfil em {contador} segundos</h3>
+      </div>
+    </>
+  );
+}
--- src/app/checkout/login/[token]/page.tsx
+++ src/app/checkout/login/[token]/page.tsx
@@ -1,22 +1,34 @@
-'use client'
-import { useEffect, useState } from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import FormLogin from "./components/formLogin";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import IsLogged from "./components/isLogged";
 
-// pages/checkout/login.js
-// import { useRouter } from 'next/router';
-
-const CheckoutLoginPage = ({ params }: { params: { token: string } }) => {
+export default function Login({ params }: { params: { token: string } }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   // const router = useRouter();
   const [tokenParams, setTokenParams] = useState<string>("");
   const [tokenSession, setTokenSession] = useState<string>("");
   const [isTokenValid, setIsTokenValid] = useState<boolean>(false);
 
+  const handleLoginSubmit = (formData: { token: string; id: number }) => {
+    sessionStorage.setItem("secretToken", formData.token);
+    sessionStorage.setItem("id", formData.id.toString());
+    setIsAuthenticated(true);
+  };
+
   useEffect(() => {
+    if (sessionStorage.getItem("secretToken")) {
+      setIsAuthenticated(true);
+    }
+
     // const searchParams = new URLSearchParams(window.location.search);
     // const token = searchParams.get("token");
     const tokenSpecial = sessionStorage.getItem('specialToken')
     // console.log(token)
-    console.log(params.token)
-    alert(params.token)
+    // console.log(params.token)
+    // alert(params.token)
 
     // Verifique se o token é uma sequência hexadecimal válida
     if (params.token === tokenSpecial) {
@@ -24,7 +36,7 @@ const CheckoutLoginPage = ({ params }: { params: { token: string } }) => {
       // O token é válido (é uma sequência hexadecimal)
       // Continue com o processo de login do checkout
       // router.push('/checkout');
-      alert('token valido')
+      // alert('token valido')
     } else {
       alert("Desculpa, você não precisa acessar essa página e estamos redirecionando você a tela de login!")
       window.location.href = "/login"
@@ -32,19 +44,42 @@ const CheckoutLoginPage = ({ params }: { params: { token: string } }) => {
 
   }, []);
 
+  // const IsLogged = () => {
+  //   window.location.href = "/cart"
+  // }
+
+
   return (
     <>
-      {isTokenValid === true ? (
-        <div>
-          <p>O token é valido</p>
+      {!isAuthenticated ? (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            {/* <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            /> */}
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Entre em sua conta
+            </h2>
+          </div>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <FormLogin onSubmit={handleLoginSubmit} />
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Não está cadastrado?{" "}
+              <a
+                href="/cadastro"
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              >
+                Cadastre-se aqui
+              </a>
+            </p>
+          </div>
         </div>
       ) : (
-        <>
-          <p>Desculpa, Você não tem acesso a essa página!</p>
-        </>
+        <IsLogged />
       )}
     </>
   );
-};
-
-export default CheckoutLoginPage;
+}
