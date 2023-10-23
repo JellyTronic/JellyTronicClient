4d6d0ee8c3563e13cbeaac25a813e219d33065a1
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-10-23 20:57:17 -0300

    fix: resolve problem orders not exist

--- src/app/minha-conta/pedidos/page.tsx
+++ src/app/minha-conta/pedidos/page.tsx
@@ -6,6 +6,8 @@ import Pedido from '@/types/Order';
 import { formatPrice } from '@/providers/formatCurrency';
 import OrderCard from './components/OrderCard';
 import Order from '@/types/Order';
+import Button from '@/components/Button';
+import Link from 'next/link';
 
 // Função para formatar o status
 
@@ -51,9 +53,23 @@ const Pedidos = () => {
           </div>
         </div>
       ) : (
-<p>
-  Você ainda não possui nenhuma compra, clique aqui e comece a comprar
-</p>
+        <div className="container mx-auto pl-2 pb-4 mt-8 bg-gray-200">
+          <div className="flex">
+            <Sidebar activeLink={'pedidos'} />
+            <div className="flex-1 p-4 bg-white rounded-md mt-4 mr-4">
+              <h1 className="text-2xl font-semibold mb-4">Meus Pedidos</h1>
+              <div className='flex flex-col items-center'>
+                <p className='mb-6'>
+                  Você ainda não possui nenhuma compra, clique no botão abaixo e comece a comprar
+                </p>
+                <Button>
+                  <Link href={'/'}>Começar a comprar</Link>
+                </Button>
+              </div>
+
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
