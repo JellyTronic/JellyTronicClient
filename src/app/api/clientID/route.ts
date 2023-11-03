fd745a01968c446bac8ce3088d44216936be6c41
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-11-03 17:48:10 -0300

    fix: remove response

--- src/app/api/clientID/route.ts
+++ src/app/api/clientID/route.ts
@@ -5,7 +5,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-08-16'
 });
 
-export async function POST(res: Response) {
+export async function POST() {
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: 1099,
