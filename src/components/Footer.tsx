d144b58d71395d7452a0eab730dd15634d7cd10f
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-11-03 12:05:49 -0300

    style: change margin footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,7 @@ const Footer = () => {
   const ano = data.getFullYear();
 
   return (
-    <div className="bg-walterWhite p-5 justify-center flex flex-col items-center">
-
+    <div className="bg-walterWhite pt-1 pb-6 justify-center flex flex-col items-center">
       <Image src='/logo.png' width={133} height={23} alt='TripWise' />
       <p className='text-sm font-medium  mt-1 text-primaryDarker'>&copy; {ano} ShopWave. Todos os direitos reservados.</p>
 
