4048954f1ef410f0a48eb63fc5868ecc47141bba
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-11-04 15:38:41 -0300

    test: remove useRoute

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Poppins } from 'next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import ToastProvider from '@/providers/toast'
-import { usePathname } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
 import HeaderPayment from './buy/components/HeaderPayment'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700', '800', '900'] })
@@ -16,8 +16,8 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 
-  const pathname = usePathname();
-  const isPaymentPage = pathname.startsWith('/buy/payments/') || pathname.startsWith('/buy/address/');
+  // const pathname = usePathname();
+  // const isPaymentPage = pathname.startsWith('/buy/payments/') || pathname.startsWith('/buy/address/');
 
   return (
     <html lang="en">
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ToastProvider>
           <div className="flex flex-col h-screen">
             <div className='h-[95px]'>
-            {isPaymentPage ? <HeaderPayment /> : <Header />}
+            {/* {isPaymentPage ? <HeaderPayment /> : <Header />} */}
               {/* <Header /> */}
             </div>
             <div className="flex-1 mt-6">{children}</div>
