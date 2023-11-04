955df075137db0b3a5548d9059ad748b2a6b4175
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-11-04 02:04:50 -0300

    refactor: change name store

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div className="bg-walterWhite pt-1 pb-6 justify-center flex flex-col items-center">
       <Image src='/logo.png' width={133} height={23} alt='TripWise' />
-      <p className='text-sm font-medium  mt-1 text-primaryDarker'>&copy; {ano} ShopWave. Todos os direitos reservados.</p>
+      <p className='text-sm font-medium  mt-1 text-primaryDarker'>&copy; {ano} JellyTronic. Todos os direitos reservados.</p>
 
     </div>
   );
